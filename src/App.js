851129a1df82757cce5e5bdf33b82fc758c8bdfc
Bennett <bennettisworking@gmail.com>
2019-04-28 00:46:59 -0400

    Added ability to mark tasks complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.scss";
-//import card_bg from './card_bg.png';
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +9,7 @@ class App extends Component {
     };
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
+    this.markComplete = this.markComplete.bind(this);
   }
   addItem(e) {
     if (this._inputElement.value !== "") {
@@ -18,6 +18,7 @@ class App extends Component {
       let newTask = {
         message: this._inputElement.value,
         color: colorArray[r],
+        completed: null,
         key: Date.now()
       };
 
@@ -26,22 +27,23 @@ class App extends Component {
           return {
             taskList: prevState.taskList.concat(newTask)
           };
-        },
-        () => {
-          console.log(this.state.taskList);
         }
       );
       this._inputElement.value = "";
     }
   }
   removeItem(a) {
-    console.log("meowww");
-    console.log(a);
-    console.log(this.state);
-    let templist = this.state.taskList.filter((item, index) => {
-      return item.key !== a;
+    let templist = this.state.taskList.filter(item  => item.key !== a);
+    //console.log(templist);
+    this.setState({
+      taskList: templist
     });
-    console.log(templist);
+  }
+  markComplete(a){
+    let templist = [...this.state.taskList];
+    let ind = this.state.taskList.findIndex(item => item.key === a);
+    templist[ind].completed = Date.now();
+    //console.log(templist);
     this.setState({
       taskList: templist
     });
@@ -68,7 +70,7 @@ class App extends Component {
           </div>
         </header>
         <section className="main">
-          <TodoItems tasks={this.state.taskList} delete={this.removeItem} />
+          <TodoItems tasks={this.state.taskList} delete={this.removeItem} complete={this.markComplete}/>
         </section>
       </div>
     );
@@ -81,10 +83,23 @@ class TodoItems extends Component {
     this.createTasks = this.createTasks.bind(this);
   }
   createTasks(task) {
+    let status = "";
+    if (task.completed!==null) {
+      let diff = task.completed - task.key;
+      
+      status = <div className="task-card__status-text task-card__status">Task completed in <br/><TimePassed diff={diff}/></div>;
+    } else {
+      status = <Button className="btn btn-sm task-card__complete task-card__status"
+      onClick={()=>{
+        this.markComplete(task.key);
+        }}
+      >I did this!</Button>;
+    }
     return (
       <div className="col-lg-3 col-md-4 col-sm-6 col-12" key={task.key}>
         <div className="task-card" style={{backgroundColor: task.color}}>
           {task.message}
+          {status}
           <i
             className="fas fa-trash-alt task-card__delete"
             onClick={() => {
@@ -98,12 +113,33 @@ class TodoItems extends Component {
   delete(key) {
     this.props.delete(key);
   }
+  markComplete(key){
+    this.props.complete(key);
+  }
   render() {
     let todoEntries = this.props.tasks;
     let listItems = todoEntries.map(this.createTasks);
 
     return <div className="row main">{listItems}</div>;
   }
+}
+
+function TimePassed(props){
+  const{diff} = props;
+  let timeString = "";
+  let timeDiff = diff;
+  const days = Math.floor(timeDiff / 86400000);
+  timeDiff -= days * 86400;
+  const hours = Math.floor(timeDiff / 3600000) % 24;
+  timeDiff -= hours * 3600;
+  const minutes = Math.floor(timeDiff / 60000) % 60;
+  timeDiff -= minutes * 60;
+  const seconds = Math.floor(timeDiff / 1000) % 60;
+  if(days){timeString+=days + " days, "}
+  if(hours){timeString+=hours + " hours, "}  
+  if(minutes){timeString+=minutes + " minutes and "}  
+  timeString+=seconds + " seconds";
+  return timeString;
 }
 
 function Button(props) {
